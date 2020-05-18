40a1e3b36616e10bb44695f8d8cc3cd667618b8b
Seonjin Park <starboy11@naver.com>
2020-05-18 13:31:23 +0000

    jot gatton geo hae gyul

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ var caoch_second = "klopp";
 var similarity_2 = 5;
 var coach_third = "klopp";
 var similarity_3 = 5;
-$("#coach_img").attr("src","coach/park.jpg");
+
+var text = 'web<br>is<br>free';
+
+
+var strTest = "테스트\n합니다.\n다시한번\n테스트 합니다.";
+
+
+var pep = "FC바르셀로나(2008~2012)\nFC바이에른 뮌헨(2013~2016)\n맨체스터시티FC(2016~ )\n리그우승 7회\nUEFA챔피언스리그우승 2회\n리그컵우승 8회";
+var result = pep.replace(/\n/g, '<p>');
+
 
 function button1_click() {
 	alert("버튼1을 누르셨습니다.");
@@ -97,8 +106,6 @@ infoArray.sort(function(a, b) { // 내림차순
 $('#slide9_coach:contains("~")').text(infoArray[0].name);
 $('#slide9_coach_p:contains("~")').text(infoArray[0].score);
 	
-/*$('#slide9_detail:contains("~")').text(infoArray[0].name);*/
-	
 $('#slide9_second:contains("~")').text(infoArray[1].name);
 $('#slide9_second_p:contains("~")').text(infoArray[1].score);
 	
@@ -106,19 +113,24 @@ $('#slide9_third:contains("~")').text(infoArray[2].name);
 $('#slide9_third_p:contains("~")').text(infoArray[2].score);
 	
 if(infoArray[0].name == "펩 과르디올라"){
-   	$('#coach_img').attr("src","coach/guardiola.jpg");
+	document.getElementById("slide9_detail").innerHTML=pep;
  }else if(infoArray[0].name == "알렉스 퍼거슨"){
 	$('#coach_img').attr("src","coach/ferguson.jpg");
+	 $('#slide9_detail:contains("~")').text(fug);
 }else if(infoArray[0].name == "슈틸리케"){
 	$('#coach_img').attr("src","coach/stielike.jpg");
+	$('#slide9_detail:contains("~")').text(sut);
 }else if(infoArray[0].name == "달글리시"){
 	$('#coach_img').attr("src","coach/dalglish.jpg");
 }else if(infoArray[0].name == "베니테즈"){
 	$('#coach_img').attr("src","coach/benitez.jpg");
+	$('#slide9_detail:contains("~")').text(ben);
 }else if(infoArray[0].name == "콘테"){
 	$('#coach_img').attr("src","coach/conte.jpg");
+	$('#slide9_detail:contains("~")').text(con);
 }else if(infoArray[0].name == "박항서"){
 	$('#coach_img').attr("src","coach/park.jpg");
+	$('#slide9_detail:contains("~")').text(par);
 }else if(infoArray[0].name == "최강희"){
 	$('#coach_img').attr("src","coach/choi.jpg");
 }else if(infoArray[0].name == "안첼로티"){
@@ -146,8 +158,6 @@ if(infoArray[0].name == "펩 과르디올라"){
 }else if(infoArray[0].name == "벵거"){
 	$('#coach_img').attr("src","coach/wenger.jpg");
 }
-
-
 infoArray[0].name;//1등
 infoArray[1].name;//2등
 infoArray[2].name;//3등
