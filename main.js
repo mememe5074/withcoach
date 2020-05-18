da67358e360b005897518d7b8ff21df4967ec6b0
Seonjin Park <starboy11@naver.com>
2020-05-18 17:24:54 +0000

    js is finished

--- main.js
+++ main.js
@@ -8,19 +8,61 @@ var caoch_second = "klopp";
 var similarity_2 = 5;
 var coach_third = "klopp";
 var similarity_3 = 5;
+var sum = 0;
 
-var text = 'web<br>is<br>free';
+var pep = "FC바르셀로나(2008~12)<br>FC바이에른 뮌헨(2013~16)<br>맨체스터시티FC(2016~ )<br>리그우승 7회<br>UEFA챔피언스리그우승 2회<br>리그컵우승 8회<br><br> 트로피 를 약속할 수는 없지만 사람들이 우리를 보고 자랑스러워하도록 만들 것이다. ";
+var fug = "맨체스터 유나이티드 FC(1986~2013)<br>프리미어리그 우승 13회<br>UEFA 챔피언스리그 우승 2회<br>FA컵 우승 5회<br><br>\“팀보다 큰 선수는 필요가 없다.\”";
+var sut = "독일대표팀(1998~2000/수석코치)<br>코트디부아르 대표팀(2006~2008)<br>대한민국 대표팀(2014~2017)<br><br>\"어떤 전술로 나갔어야 할지 내가 묻고 싶다.\"";
+var ben = "발렌시아CF(2001~04)<br>리버풀FC(2004~10)<br>인테르(2010~11)<br>첼시FC(2012~13)<br>나폴리(2013~15)<br>레알 마드리드(2015~16)<br>리그우승 2회<br>UEFA챔피언스리그 우승 1회<br>UEFA 유로파리그우승 1회<br>리그컵 우승 3회<br>\
+<br>우리가 할 수 있다고 믿으면, 우리는 그렇게 할 것이다.";
+var con = "유벤투스FC (2011~14)<br>이탈리아대표팀 (2014~16)<br>첼시FC (2016~18)<br>FC인테르나치오날레 밀라노(2019~ )<br>리그우승 4회<br>리그컵 3회<br>\“패배는 죽음을 선언한 것과 다름이 없다.\”";
+var par = "베트남 대표팀(2017~ )<br>베트남U-23 대표팀(2017~ )<br>스즈키컵 10년 만에 우승<br>아시안컵 최초 8강<br>동남아시안게임 60년 만에 우승<br>\"우리는 최선을 다했기 때문에 자부심을 가져도 된다.\"";
 
 
-var strTest = "테스트\n합니다.\n다시한번\n테스트 합니다.";
+$(function() {
+	sum++;
+  $('.slick-next').click( function() {
+	sum++;
+	  console.log(sum);
+	  if(sum == 8){
+		$(".slick-next").hide();
+		$(".slick-arrow").hide();
+	}
+	});
+	
+});
 
+$(function() {
+  $('.slick-prev').click( function() {
+	sum--;
+	  console.log(sum);
+	});
+	
+});
+console.log(sum);
 
-var pep = "FC바르셀로나(2008~2012)\nFC바이에른 뮌헨(2013~2016)\n맨체스터시티FC(2016~ )\n리그우승 7회\nUEFA챔피언스리그우승 2회\n리그컵우승 8회";
-var result = pep.replace(/\n/g, '<p>');
-
+window.onload = function () {
+	$(".slick-next").hide();
+	$(".slick-arrow").hide();
+}
 
 function button1_click() {
-	alert("버튼1을 누르셨습니다.");
+	$(".slick-next").show();
+	$(".slick-arrow").show();
+	
+	$(".slick-next").trigger("click");
+	$(".slick-arrow").trigger("click");
+}
+
+
+function button2_click() {
+	$(".slick-next").hide();
+	$(".slick-arrow").hide();
+	
+	
+	alert("입력하신대로 분석중입니다.");
+	$(".slick-next").trigger("click");
+	
 	$("input[name='q1']:checked").each(function(){
 	answer[0] = $(this).val();
 });
@@ -113,24 +155,32 @@ $('#slide9_third:contains("~")').text(infoArray[2].name);
 $('#slide9_third_p:contains("~")').text(infoArray[2].score);
 	
 if(infoArray[0].name == "펩 과르디올라"){
+	$('#coach_img').attr("src","coach/guardiola.jpg");
 	document.getElementById("slide9_detail").innerHTML=pep;
+	
  }else if(infoArray[0].name == "알렉스 퍼거슨"){
 	$('#coach_img').attr("src","coach/ferguson.jpg");
-	 $('#slide9_detail:contains("~")').text(fug);
+	document.getElementById("slide9_detail").innerHTML=fug;
+	 
 }else if(infoArray[0].name == "슈틸리케"){
 	$('#coach_img').attr("src","coach/stielike.jpg");
-	$('#slide9_detail:contains("~")').text(sut);
+	document.getElementById("slide9_detail").innerHTML=sut;
+	
 }else if(infoArray[0].name == "달글리시"){
 	$('#coach_img').attr("src","coach/dalglish.jpg");
+	
 }else if(infoArray[0].name == "베니테즈"){
 	$('#coach_img').attr("src","coach/benitez.jpg");
-	$('#slide9_detail:contains("~")').text(ben);
+	document.getElementById("slide9_detail").innerHTML=ben;
+	
 }else if(infoArray[0].name == "콘테"){
 	$('#coach_img').attr("src","coach/conte.jpg");
-	$('#slide9_detail:contains("~")').text(con);
+	document.getElementById("slide9_detail").innerHTML=con;
+	
 }else if(infoArray[0].name == "박항서"){
 	$('#coach_img').attr("src","coach/park.jpg");
-	$('#slide9_detail:contains("~")').text(par);
+	document.getElementById("slide9_detail").innerHTML=par;
+	
 }else if(infoArray[0].name == "최강희"){
 	$('#coach_img').attr("src","coach/choi.jpg");
 }else if(infoArray[0].name == "안첼로티"){
@@ -158,8 +208,5 @@ if(infoArray[0].name == "펩 과르디올라"){
 }else if(infoArray[0].name == "벵거"){
 	$('#coach_img').attr("src","coach/wenger.jpg");
 }
-infoArray[0].name;//1등
-infoArray[1].name;//2등
-infoArray[2].name;//3등
 }
 
