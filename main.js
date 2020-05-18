a7526bed256cc3ce9c5be52a5424fa54ee6cb4e1
Seonjin Park <starboy11@naver.com>
2020-05-18 11:23:54 +0000

    final

--- main.js
+++ main.js
@@ -1,41 +1,48 @@
 
-var formation, manage, media, attack, defence, rotation, key;
-var answer = [formation, manage, media, attack, defence, rotation, key];
+var answer = [0, 0, 0, 0, 0, 0, 0];
 
-$("input[name='q1']:checked").each(function(){
+var caoch_name = "klopp";
+var coach_detail = "awsome";
+var similarity = 5;
+var caoch_second = "klopp";
+var similarity_2 = 5;
+var coach_third = "klopp";
+var similarity_3 = 5;
+$("#coach_img").attr("src","coach/park.jpg");
+
+function button1_click() {
+	alert("버튼1을 누르셨습니다.");
+	$("input[name='q1']:checked").each(function(){
 	answer[0] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q2']:checked").each(function(){
 	answer[1] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q3']:checked").each(function(){
 	answer[2] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q4']:checked").each(function(){
 	answer[3] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q5']:checked").each(function(){
 	answer[4] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q6']:checked").each(function(){
 	answer[5] = $(this).val();
-	console.log($(this).val());
 });
 
 $("input[name='q7']:checked").each(function(){
 	answer[6] = $(this).val();
-	console.log($(this).val());
 });
+
+for(var i=0; i<7;i++){
+	console.log(answer[i]);
+}
 
 var infoArray = [];
 var sortingField = "score";
@@ -60,33 +67,89 @@ var sortingField = "score";
 	infoArray.push({ name : "뢰브", formation:"3", manage:"4" , media:"4" , attack:"1" , defence:"2" , rotation:"1" , key:"3", score:0  });
 	infoArray.push({ name : "벵거", formation:"1", manage:"3" , media:"4" , attack:"1" , defence:"1" , rotation:"1" , key:"3", score:0  });
 
-for(let i ; i<20; i++){
-	if (answers[0] == coach[i].formation) {
-		coach[i].score += 10;
+for(var i=0 ; i<20; i++){
+	if (answer[0] == infoArray[i].formation) {
+		infoArray[i].score += 10;
 		}
-	if (answers[1] == coach[i].manage) {
-		coach[i].score += 10;
+	if (answer[1] == infoArray[i].manage) {
+		infoArray[i].score += 10;
 	}
-	if (answers[2] == coach[i].media) {
-		coach[i].score += 10;
+	if (answer[2] == infoArray[i].media) {
+		infoArray[i].score += 10;
 	}
-	if (answers[3] == coach[i][3].attack) {
-		coach[i].score += 20;
+	if (answer[3] == infoArray[i].attack) {
+		infoArray[i].score += 20;
 	}
-	if (answers[4] == coach[i][4].defence) {
-		coach[i].score += 20;
+	if (answer[4] == infoArray[i].defence) {
+		infoArray[i].score += 20;
 	}
-	if (answers[5] == coach[i][5].rotation) {
-		coach[i].score += 10;
+	if (answer[5] == infoArray[i].rotation) {
+		infoArray[i].score += 10;
 	}
-	if (answers[6] == coach[i][6].key) {
-		coach[i].score += 20;
+	if (answer[6] == infoArray[i].key) {
+		infoArray[i].score += 20;
 	}
 }
 infoArray.sort(function(a, b) { // 내림차순
     return b[sortingField] - a[sortingField];
 });
 
+$('#slide9_coach:contains("~")').text(infoArray[0].name);
+$('#slide9_coach_p:contains("~")').text(infoArray[0].score);
+	
+/*$('#slide9_detail:contains("~")').text(infoArray[0].name);*/
+	
+$('#slide9_second:contains("~")').text(infoArray[1].name);
+$('#slide9_second_p:contains("~")').text(infoArray[1].score);
+	
+$('#slide9_third:contains("~")').text(infoArray[2].name);
+$('#slide9_third_p:contains("~")').text(infoArray[2].score);
+	
+if(infoArray[0].name == "펩 과르디올라"){
+   	$('#coach_img').attr("src","coach/guardiola.jpg");
+ }else if(infoArray[0].name == "알렉스 퍼거슨"){
+	$('#coach_img').attr("src","coach/ferguson.jpg");
+}else if(infoArray[0].name == "슈틸리케"){
+	$('#coach_img').attr("src","coach/stielike.jpg");
+}else if(infoArray[0].name == "달글리시"){
+	$('#coach_img').attr("src","coach/dalglish.jpg");
+}else if(infoArray[0].name == "베니테즈"){
+	$('#coach_img').attr("src","coach/benitez.jpg");
+}else if(infoArray[0].name == "콘테"){
+	$('#coach_img').attr("src","coach/conte.jpg");
+}else if(infoArray[0].name == "박항서"){
+	$('#coach_img').attr("src","coach/park.jpg");
+}else if(infoArray[0].name == "최강희"){
+	$('#coach_img').attr("src","coach/choi.jpg");
+}else if(infoArray[0].name == "안첼로티"){
+	$('#coach_img').attr("src","coach/anchelotti.jpg");
+}else if(infoArray[0].name == "무리뉴"){
+	$('#coach_img').attr("src","coach/mourinho.jpg");
+}else if(infoArray[0].name == "지단"){
+	$('#coach_img').attr("src","coach/zidane.jpg");
+}else if(infoArray[0].name == "사리"){
+	$('#coach_img').attr("src","coach/sarri.jpg");
+}else if(infoArray[0].name == "포체티노"){
+	$('#coach_img').attr("src","coach/pochettino.jpg");
+}else if(infoArray[0].name == "클롭"){
+	$('#coach_img').attr("src","coach/klopp.jpg");
+}else if(infoArray[0].name == "시메오네"){
+	$('#coach_img').attr("src","coach/simeone.jpg");
+}else if(infoArray[0].name == "하인케스"){
+	$('#coach_img').attr("src","coach/heynckes.jpg");
+}else if(infoArray[0].name == "히딩크"){
+	$('#coach_img').attr("src","coach/hidink.jpg");
+}else if(infoArray[0].name == "알레그리"){
+	$('#coach_img').attr("src","coach/allegri.jpg");
+}else if(infoArray[0].name == "뢰브"){
+	$('#coach_img').attr("src","coach/low.jpg");
+}else if(infoArray[0].name == "벵거"){
+	$('#coach_img').attr("src","coach/wenger.jpg");
+}
+
+
 infoArray[0].name;//1등
 infoArray[1].name;//2등
 infoArray[2].name;//3등
+}
+
