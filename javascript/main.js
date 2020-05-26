ea23cdcd5400cd4689a2354d0d3e5e9f45c417bf
Seonjin Park <starboy11@naver.com>
2020-05-26 12:06:53 +0000

    click!

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,11 @@
+function clickEffect(e){
+  var d=document.createElement("div");
+  d.className="clickEffect";
+  d.style.top=e.clientY+"px";d.style.left=e.clientX+"px";
+  document.body.appendChild(d);
+  d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
+}
+	document.addEventListener('click',clickEffect);	
 $(document).ready(function(){
 				$(document).bind('keydown',function(e){
 					if ( e.keyCode == 123 /* F12 */) {
